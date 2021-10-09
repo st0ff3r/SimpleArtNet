0ac3c4ec9a43d8a525ad192a0257c748f2556e42
Kristoffer Ek <stoffer@skulp.net>
2021-10-09 19:04:27 +0200

    working

---
+++ movie_to_artnet.pl
@@ -0,0 +1,48 @@
+#! /usr/bin/perl -w
+
+use File::Temp qw( tempfile tempdir );
+use File::Copy;
+use Image::Magick;
+use Image::Size;
+use Data::Dumper;
+
+use constant NUM_PIXELS => 30;
+
+my $movie_file = $ARGV[0];
+my $artnet_data_file = $ARGV[1] || "artnet.data";
+
+my $temp_dir = tempdir( CLEANUP => 1 );
+
+# convert movie to images
+#if (system("ffmpeg -i " . $movie_file . " -vf scale=" . NUM_PIXELS . ":-1 " . $temp_dir . "/%05d.png") != 0) {
+if (system("ffmpeg -loglevel -8 -i " . $movie_file . " -vf scale=" . NUM_PIXELS . ":-1 " . $temp_dir . "/%05d.png") != 0) {
+	die "system failed: $?";
+}
+
+# get all images
+opendir(DIR, $temp_dir) || die "can't opendir $temp_dir: $!";
+my @images = grep { -f "$temp_dir/$_" } readdir(DIR);
+closedir DIR;
+
+my ($image_size_x, $image_size_y);
+my $x;
+my ($fh, $temp_file) = tempfile( CLEANUP => 0 );
+my ($red, $green, $blue);
+
+foreach (sort @images) {
+	($image_size_x, $image_size_y) = imgsize("$temp_dir/$_");
+	
+	my $p = new Image::Magick;
+	$p->Read("$temp_dir/$_");
+	for $x (0..$image_size_x) {
+		($red, $green, $blue) = $p->GetPixel( 'x' => $x, 'y' => int($image_size_y / 2) );
+		print $fh sprintf("%02x", int($red * 255)) . sprintf("%02x", int($green * 255)) . sprintf("%02x", int($blue * 255));
+	}
+	print $fh "\n";
+}
+close($fh);
+move($temp_file, $artnet_data_file) || die $!;
+
+1;
+
+__END__
---
+++ send_artnet_data.pl
@@ -0,0 +1,45 @@
+#! /usr/bin/perl -w
+
+use Artnet;
+use IO::Socket::INET;
+use Time::HiRes qw(usleep gettimeofday tv_interval);
+use Data::Dumper;
+
+use constant PEER_ADDR => '192.168.6.18';
+#use constant PEER_ADDR => '127.0.0.1';
+use constant UNIVERSE => 0;
+
+use constant SPEED => 40_000;
+
+my $artnet_data_file = $ARGV[0];
+
+# network connection
+my $a = new Artnet(
+	peer_addr => PEER_ADDR
+);
+
+my @pixel_line;
+my ($red, $green, $blue);
+while (1) {
+	open(FH, '<', $artnet_data_file) or die $!;
+	while(<FH>){
+		@pixel_line = (/.{2}/g);
+		my $i = 0;
+		while (($red, $green, $blue) = splice(@pixel_line, 0, 3)) {
+			$a->set_pixel(
+				pixel => $i,
+				red => hex($red) / 255,
+				green => hex($green) / 255,
+				blue => hex($blue) / 255,
+			);
+			$i++;
+		}
+		$a->send_artnet();
+		usleep(20_000);
+	}
+	close(FH);
+}
+
+1;
+
+__END__
