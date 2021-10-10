9dbc501cd5f8d4b25bb81cab7cf5b6dc55ae418d
Kristoffer Ek <stoffer@skulp.net>
2021-10-10 19:44:58 +0200

    moved gamma correction to Artnet.pm
    + use config file artnet.conf

--- Artnet.pm
+++ Artnet.pm
@@ -5,6 +5,25 @@ use Data::Dumper;
 use constant NUM_CHANNELS_PER_PIXEL => 3;
 use constant PIXEL_FORMAT => 'GRB';
 
+my @gamma_table = (
+	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+	1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+	2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+	5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+	10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+	17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+	25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+	37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+	51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+	69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+	90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+	115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+	144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+	177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+	215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
+);
+
 # flush after every write
 $| = 1;
 
@@ -38,9 +57,9 @@ sub set_pixel {
 	
 	if ($pixel * NUM_CHANNELS_PER_PIXEL <= 512) {
 		if (PIXEL_FORMAT eq 'GRB') {
-			vec($self->{dmx_channels}, $pixel * NUM_CHANNELS_PER_PIXEL + 0, 8) = int(0xff * $green);
-			vec($self->{dmx_channels}, $pixel * NUM_CHANNELS_PER_PIXEL + 1, 8) = int(0xff * $red);
-			vec($self->{dmx_channels}, $pixel * NUM_CHANNELS_PER_PIXEL + 2, 8) = int(0xff * $blue);
+			vec($self->{dmx_channels}, $pixel * NUM_CHANNELS_PER_PIXEL + 0, 8) = gamma_correction(int(0xff * $green));
+			vec($self->{dmx_channels}, $pixel * NUM_CHANNELS_PER_PIXEL + 1, 8) = gamma_correction(int(0xff * $red));
+			vec($self->{dmx_channels}, $pixel * NUM_CHANNELS_PER_PIXEL + 2, 8) = gamma_correction(int(0xff * $blue));
 		}
 	}
 }
@@ -50,6 +69,11 @@ sub send_artnet {
 
 	my $packet = "Art-Net\x00\x00\x50\x00\x0e\x00\x00" . chr($self->{universe}) . "\x00" . chr(2) . chr(0) . $self->{dmx_channels};
 	$self->{socket}->send($packet);
+}
+
+# private functions
+sub gamma_correction {
+	return $gamma_table[shift];
 }
 
 1;
--- send_artnet_data.pl
+++ send_artnet_data.pl
@@ -2,42 +2,20 @@
 
 use IO::Socket::INET;
 use Time::HiRes qw(usleep gettimeofday tv_interval);
+use Config::Simple;
 use Data::Dumper;
 
 use lib qw ( ./ );
 use Artnet;
 
-#use constant PEER_ADDR => '192.168.6.18';
-use constant PEER_ADDR => '10.0.1.95';
-#use constant PEER_ADDR => '127.0.0.1';
-use constant UNIVERSE => 0;
+use constant ARTNET_CONF => 'artnet.conf';
 
-use constant SPEED => 40_000;
-
-my @gamma_table = (
-	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-	1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-	2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-	5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-	10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-	17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-	25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-	37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-	51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-	69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-	90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-	115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-	144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-	177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-	215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
-);
-
+my $config = new Config::Simple(ARTNET_CONF);
 my $artnet_data_file = $ARGV[0];
 
 # network connection
 my $a = new Artnet(
-	peer_addr => PEER_ADDR
+	peer_addr => $config->param('peer_addr')
 );
 
 my @pixel_line;
@@ -50,14 +28,14 @@ while (1) {
 		while (($red, $green, $blue) = splice(@pixel_line, 0, 3)) {
 			$a->set_pixel(
 				pixel => $i,
-				red => $gamma_table[hex($red)] / 255,
-				green => $gamma_table[hex($green)] / 255,
-				blue => $gamma_table[hex($blue)] / 255,
+				red => hex($red) / 255,
+				green => hex($green) / 255,
+				blue => hex($blue) / 255,
 			);
 			$i++;
 		}
 		$a->send_artnet();
-		usleep(SPEED);
+		usleep($config->param('speed'));
 	}
 	close(FH);
 }
