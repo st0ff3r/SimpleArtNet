a9878a64fbf19a3b61dce2df7751d0f74655dd3a
Kristoffer Ek <stoffer@skulp.net>
2021-10-11 03:54:36 +0200

    Update led_control.pl

--- led_control.pl
+++ led_control.pl
@@ -9,11 +9,13 @@ use constant ARTNET_CONF => 'artnet.conf';
 my $config = new Config::Simple(ARTNET_CONF);
 
 my $intensity = $ARGV[0];
+my $pid = $ARGV[1] || die "need pid of send_artnet_data process\n";
 
 my $share = IPC::ShareLite->new(
 	-key		=> 6454,
 	-create		=> 'yes',
-	-destroy	=> 'yes'
+	-destroy	=> 'no'
 ) or die $!;
 
 $share->store($intensity);
+kill('HUP', $pid);
