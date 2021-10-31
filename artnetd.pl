ef905e8caeeda2309781efa05ac06bb97fb05377
Kristoffer Ek <stoffer@skulp.net>
2021-10-31 21:57:32 +0100

    adjusting fps using proportionally method

--- artnetd.pl
+++ artnetd.pl
@@ -13,6 +13,8 @@ use constant REDIS_QUEUE_NAME => 'artnet';
 
 use constant ARTNET_CONF => 'artnet.conf';
 
+use constant PID_I => 1000_000;
+
 my $config = new Config::Simple(ARTNET_CONF);
 
 my $redis_host = REDIS_HOST;
@@ -25,11 +27,21 @@ my $queue_name = 'artnet';
 my $timeout		= 86400;
 
 my @stats = ();
+my $fps_adjustment = 0;
+my $avg_fps;
+my $fps;
 
 # print fps stats
 $SIG{HUP} = sub { 
-	my $avg_fps = @stats / tv_interval($stats[0], $stats[-1]);
-	print "avg_fps: $avg_fps\n";
+	if (@stats >= $fps) {
+		print "avg_fps: $avg_fps\n";
+#		print "fps_adjustment: $fps_adjustment\n";
+		print 'PID_I * ($avg_fps - $fps)' . "\n";
+		print PID_I . ' * ' . '(' . $avg_fps . ' - ' . $fps . ')' . ' = ' . $fps_adjustment . "\n\n";
+	}
+	else {
+		print "cant calculate stats yet\n";
+	}
 };
 
 # flush after every write
@@ -53,14 +65,21 @@ while (1) {
 		$redis->del($job_id);
 	}
 	
-	my $fps = $redis->get('fps');
+	$fps = $redis->get('fps');
 
+	# update fps stats
 	if (@stats > $fps * 60) {	# a time window of a minute for stats
 		shift @stats;
 	}
 	push @stats, $time;
+	
+	# adjust fps (proportionally - debug: should we do I and D too?)
+	if (@stats >= $fps) {
+		$avg_fps = @stats / tv_interval($stats[0], $stats[-1]);
+		$fps_adjustment = PID_I * ($avg_fps - $fps);
+	}
 
-	my $usleep_time = 1000_000 * ((1 / $fps) - tv_interval($time));
+	my $usleep_time = 1000_000 * ((1 / $fps) - tv_interval($time)) + $fps_adjustment;
 	usleep($usleep_time >= 0 ? $usleep_time : 0);
 }
 
