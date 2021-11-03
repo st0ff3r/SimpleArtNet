d6001e04d66e24c4cb800798cf66a9086770121f
Kristoffer Ek <stoffer@skulp.net>
2021-11-03 21:05:45 +0100

    precalculate mirrored artnet

--- LedController/Artnet.pm
+++ LedController/Artnet.pm
@@ -52,8 +52,13 @@ sub new {
 
 	# init all channels to 0
 	$self->{num_universes} = ceil($self->{num_pixels} * $self->{num_channels_per_pixel} / 512);
+	# on first port
 	for (1..$self->{num_universes}) {
 		$self->{dmx_channels}[$_ - 1] = chr(0) x 512;
+	}
+	# on second port
+	for (1..$self->{num_universes}) {
+		$self->{dmx_channels_mirrored}[$_ - 1] = chr(0) x 512;
 	}
 	
 	# calculate channel padding
@@ -90,17 +95,30 @@ sub set_pixel {
 	
 	my $channel = ($pixel * $self->{num_channels_per_pixel}) % 512;
 	my $universe = int($pixel * $self->{num_channels_per_pixel} / 512);
+
+	my $channel_mirrored = (($self->{num_pixels} - $pixel) * $self->{num_channels_per_pixel}) % 512;
+	my $universe_mirrored = int(($self->{num_pixels} - $pixel) * $self->{num_channels_per_pixel} / 512);
+
 #	print('pixel: ' . $pixel . ' => ' . 'universe: ' . $universe . ', channel: ' . $channel . "\n");
 	if ($self->{pixel_format} eq 'GRB') {
 		vec($self->{dmx_channels}[$universe], $channel + 0, 8) = $green;
 		vec($self->{dmx_channels}[$universe], $channel + 1, 8) = $red;
 		vec($self->{dmx_channels}[$universe], $channel + 2, 8) = $blue;
+
+		vec($self->{dmx_channels_mirrored}[$universe_mirrored], $channel_mirrored + 0, 8) = $green;
+		vec($self->{dmx_channels_mirrored}[$universe_mirrored], $channel_mirrored + 1, 8) = $red;
+		vec($self->{dmx_channels_mirrored}[$universe_mirrored], $channel_mirrored + 2, 8) = $blue;
 	}
 	elsif ($self->{pixel_format} eq 'GRBW') {
 		vec($self->{dmx_channels}[$universe], $channel + 0, 8) = $green;
 		vec($self->{dmx_channels}[$universe], $channel + 1, 8) = $red;
 		vec($self->{dmx_channels}[$universe], $channel + 2, 8) = $blue;
 		vec($self->{dmx_channels}[$universe], $channel + 3, 8) = $white;
+
+		vec($self->{dmx_channels_mirrored}[$universe_mirrored], $channel_mirrored + 0, 8) = $green;
+		vec($self->{dmx_channels_mirrored}[$universe_mirrored], $channel_mirrored + 1, 8) = $red;
+		vec($self->{dmx_channels_mirrored}[$universe_mirrored], $channel_mirrored + 2, 8) = $blue;
+		vec($self->{dmx_channels_mirrored}[$universe_mirrored], $channel_mirrored + 3, 8) = $white;
 	}
 }
 
@@ -117,29 +135,17 @@ sub send_artnet {
 		push @$frame, $packet;
 	}
 	$self->add_artnet_to_queue(queue => REDIS_QUEUE_1_NAME, artnet => freeze($frame), fps => $p{fps});
-	
+
 	# send mirrored data to other port
-	my $num_channels_per_pixel = $self->{num_channels_per_pixel};
-	my $num_channels_not_used = $self->{num_channels_not_used};
 	$frame = [];
 	for (1..$self->{num_universes}) {
-		my $mirrored_dmx_channels = $self->{dmx_channels}[$_ - 1];
-		
-		if ($_ == $self->{num_universes}) {
-			# if last universe
-			$mirrored_dmx_channels =~ s/.{$num_channels_not_used}$//s;	# remove channel padding		
-			$mirrored_dmx_channels = join(q[], reverse($mirrored_dmx_channels =~ /(.{$num_channels_per_pixel})/gs));	# mirror whole universe
-			$mirrored_dmx_channels .= chr(0) x $num_channels_not_used;	# add channel padding again
-			
-		}
-		else {
-			$mirrored_dmx_channels = join(q[], reverse($mirrored_dmx_channels =~ /(.{$num_channels_per_pixel})/gs));	# mirror whole universe
-		}
-
-		$packet = "Art-Net\x00\x00\x50\x00\x0e\x00\x00" . chr($_ - 1 + $self->{universes_per_port}) . "\x00" . chr(2) . chr(0) . $mirrored_dmx_channels;
+		$packet = "Art-Net\x00\x00\x50\x00\x0e\x00\x00" . chr($_ - 1 + $self->{num_universes}) . "\x00" . chr(2) . chr(0) . $self->{dmx_channels_mirrored}[$_ - 1];
 		push @$frame, $packet;
 	}
 	$self->add_artnet_to_queue(queue => REDIS_QUEUE_2_NAME, artnet => freeze($frame), fps => $p{fps});
+	
+#	my $num_channels_per_pixel = $self->{num_channels_per_pixel};
+#	my $num_channels_not_used = $self->{num_channels_not_used};
 	
 	# wait for buffer to be emptied
 	while ($self->{redis}->keys(REDIS_QUEUE_1_NAME . ':*') > (BUFFER_TIME * $p{fps})) {
